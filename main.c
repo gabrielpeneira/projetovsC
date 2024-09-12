8bdeebbf7c7353b7f5203cad24362b4bd1ae6d31
gabriel pereira <gabriel.pereira12321@gmail.com>
2024-09-12 11:56:32 -0300

    teste

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+    float paulo;
+    float passaro; 
+    float pitaya;
+    
+    paulo = 1.15;
+    passaro = 217;
+    pitaya = paulo / passaro;
+
+    printf("paulo somado com passaro eh = %.3f ",pitaya);
+
+    return 0;
+}
