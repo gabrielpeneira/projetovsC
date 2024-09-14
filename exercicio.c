00e7d34c2013182cb42c7303bf3d888ec3392cc2
gabriel pereira <gabriel.pereira12321@gmail.com>
2024-09-14 11:37:24 -0300

    refeito

--- exercicio.c
+++ exercicio.c
@@ -8,13 +8,22 @@ float calculaReajuste(float valorAtual, float valorReajuste){
 
     return percentual;
 }
+float recalculaReajuste(float valorAtual){
 
-void exibeReajuste(float percentual){
+    float percentual;
+
+    percentual = 0.07 * valorAtual;
+
+    return percentual + valorAtual;
+
+}
+
+void exibeReajuste(float percentual, float recalculo){
 
     if(percentual<=7){
 
         printf("o resjuste da passagem eh de %f %%",percentual);
-    }else printf("este reajuste eh inconstitucioanal");
+    }else printf("este reajuste eh inconstitucioanal, o valor maximo permitido para esse valor eh: %.2f reais",recalculo);
 }
 
 int main()
@@ -27,7 +36,7 @@ int main()
     printf("digite o valor reajustado da passagem :\n");
     scanf("%f",&valorReajuste);
 
-    exibeReajuste(calculaReajuste(valorAtual,valorReajuste));
+    exibeReajuste(calculaReajuste(valorAtual,valorReajuste),recalculaReajuste(valorAtual));
     
     return 0;
 }
