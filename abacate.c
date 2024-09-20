eee8cb1999803979e60fccc57f041df427ed807c
gabriel pereira <gabriel.pereira12321@gmail.com>
2024-09-19 21:16:14 -0300

    praga

---
+++ abacate.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*faca um progamra que Faça um programa que informe quantos pontos um time de futebol possui no Brasileirão, sabendo que o número
+de vitórias, derrotas e empates do time serão fornecidos. */
+int main ()
+{
+
+int empates;
+int vitorias;
+int pontos;
+
+printf("coantous epate e vitora s eu time te (repectsivalmente)");
+scanf("%i %i",&empates,&vitorias);
+
+pontos =  + vitorias * 3 + empates;
+
+printf("seo tim ten %i pontos",pontos);
+
+    return 0;
+}
